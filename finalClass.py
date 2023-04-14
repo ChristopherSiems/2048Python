123ea43d5eea0a40ad0cdbba2dbf7618b6bb08c5
gitopher <65359837+gitopherhub@users.noreply.github.com>
2023-04-14 14:57:27 -0400

    Merge branch 'main' of https://github.com/gitopherhub/CSCI120-Final

--- finalClass.py
+++ finalClass.py
@@ -1,21 +1,47 @@
+import random
+
 class App2048:
-    #initializing board
-    def __init__(self):
+    
+    def __init__(self): #initializing board
         self.board = [[0] * 4] * 4
 
     def __repr__(self):
-        print(*self.board, sep = " ")
+        string = f'''{self.board[0][0]} {self.board[1][0]} {self.board[2][0]} {self.board[3][0]}
+{self.board[0][1]} {self.board[1][1]} {self.board[2][1]} {self.board[3][1]} 
+{self.board[0][2]} {self.board[1][2]} {self.board[2][2]} {self.board[3][2]}
+{self.board[0][3]} {self.board[1][3]} {self.board[2][3]} {self.board[3][3]}
+'''
+        return string
 
-    def right(self):
+    def define(self, b):
+        self.board = b
+    
+    def step(self):
+        assigned = False
+        while not assigned:
+            x = random.randrange(0, 4)
+            y = random.randrange(0, 4)
+            print(x)
+            print(y)
+            if self.board[x][y] == 0:
+                self.board[x][y] = 2
+                assigned = True
+
+    '''def up(self):
+        for ix, column in enumerate(self.board):
+            for iy, num in enumerate(column):
+                if iy != 0:
+                    for other in column[:iy:-1]:
+                        if num == other:'''
+
+    '''def right(self):
         for column in range(4):
             for row in range(4):
                 if self.board[row][column] == 0:
                     for num in range(row,-1):
-                        if num != 0:
-
-
-
-                
+                        if num != 0:'''
 
 game1 = App2048()
+print(game1)
+game1.step()
 print(game1)
