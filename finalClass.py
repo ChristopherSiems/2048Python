e74e3871549689b6173c5b28ad1f44ce84bdab4f
Sai Nikhil Chanda <130773971+SaiNikhilChanda@users.noreply.github.com>
2023-04-14 11:40:16 -0400

    working on the right function

--- finalClass.py
+++ finalClass.py
@@ -1,5 +1,21 @@
-class Board:
+class App2048:
+    #initializing board
     def __init__(self):
         self.board = [[0] * 4] * 4
+
     def __repr__(self):
-        print(self.board)
+        print(*self.board, sep = " ")
+
+    def right(self):
+        for column in range(4):
+            for row in range(4):
+                if self.board[row][column] == 0:
+                    for num in range(row,-1):
+                        if num != 0:
+
+
+
+                
+
+game1 = App2048()
+print(game1)
