007cce22fb7424b277c470aac9623e8142f9632f
gitopher <65359837+gitopherhub@users.noreply.github.com>
2023-04-20 23:39:50 -0400

    full commands

--- game_logic.py
+++ game_logic.py
@@ -52,20 +52,17 @@ class App2048:
                     self.board[y - shift][x] = self.board[y][x]
                     self.board[y][x] = 0
         for x in range(0, 4):
-            for y in range(3, 0, -1):
-                if self.board[y][x] != 0 and self.board[y][x] == self.board[y - 1][x]:
-                    self.board[y - 1][x] *= 2
-                    self.board[y][x] = 0
+            for y in range(0, 4):
+                if self.board[y][x] != 0 and self.board[y][x] == self.board[y + 1][x]:
+                    self.board[y][x] *= 2
+                    self.board[y + 1][x] = 0
 
     def downMove(self):   
-        '''this function will move up'''
+        '''this function will move down'''
         for x in range(0, 4):
             shift = 0
             for y in range(3, -1, -1):
-                if self.board[y][x] == 0 and y == 0:
-                    self.board[y + shift][x] = self.board[y][x]
-                    self.board[y][x] = 0
-                elif self.board[y][x] == 0:
+                if self.board[y][x] == 0:
                     shift += 1
                 elif shift == 0:
                     continue
@@ -73,19 +70,14 @@ class App2048:
                     self.board[y + shift][x] = self.board[y][x]
                     self.board[y][x] = 0
         for x in range(0, 4):
-            for y in range(3, -1, -1):
-                if y == 3:
-                    if self.board[y][x] != 0 and self.board[y][x] == self.board[2][x]:
-                        self.board[y][x] *= 2
-                        self.board[2][x] = 0
-                elif self.board[y][x] != 0 and self.board[y][x] == self.board[y + 1][x]:
-                    self.board[y + 1][x] *= 2
-                    self.board[y][x] = 0
-
+            for y in range(3, 0, -1):
+                if self.board[y][x] != 0 and self.board[y][x] == self.board[y - 1][x]:
+                    self.board[y][x] *= 2
+                    self.board[y - 1][x] = 0
     
     def leftMove(self):
-        '''this function will move up'''
-        for y in range(0,4):
+        '''this function will move left'''
+        for y in range(0, 4):
             shift = 0
             for x in range(0, 4):
                 if self.board[y][x] == 0:
@@ -95,16 +87,17 @@ class App2048:
                 else:
                     self.board[y][x - shift] = self.board[y][x]
                     self.board[y][x] = 0
+        for y in range(0, 4):
+            for x in range(0, 4):
                 if self.board[y][x] != 0 and self.board[y][x] == self.board[y][x + 1]:
                     self.board[y][x] *= 2
                     self.board[y][x + 1] = 0
-    
 
     def rightMove(self):
-        '''this function goes left'''
-        for y in range(3, 0,-1):
+        '''this function goes right'''
+        for y in range(0, 4):
             shift = 0
-            for x in range(3,-1, -1):
+            for x in range(3, -1, -1):
                 if self.board[y][x] == 0:
                     shift += 1
                 elif shift == 0:
@@ -112,17 +105,37 @@ class App2048:
                 else:
                     self.board[y][x + shift] = self.board[y][x]
                     self.board[y][x] = 0
-                    if self.board[y][x-1] != 0 and x >= 0:
-                        self.board[y][x + shift - 1] = self.board[y][x - 1]
-                        self.board[y][x - 1] = 0
-                if self.board[y][x] != 0 and self.board[y][x] == self.board[y - 1][x]:
-                    self.board[y - 1][x] *= 2
-                    self.board[y][x] = 0
+        for y in range(0, 4):
+            for x in range(3, 0, -1):
+                if self.board[y][x] != 0 and self.board[y][x] == self.board[y][x - 1]:
+                    self.board[y][x] *= 2
+                    self.board[y][x - 1] = 0
+    
+    def fullUp(self):
+        self.upMove()
+        self.upMove()
+        self.upMove()
+        self.pickTwoOrFour()
+    
+    def fullDown(self):
+        self.downMove()
+        self.downMove()
+        self.downMove()
+        self.pickTwoOrFour()
+    
+    def fullLeft(self):
+        self.leftMove()
+        self.leftMove()
+        self.leftMove()
+        self.pickTwoOrFour()
+    
+    def fullRight(self):
+        self.rightMove()
+        self.rightMove()
+        self.rightMove()
+        self.pickTwoOrFour()
                 
-        
-
-
-game1 = App2048()
+'''game1 = App2048()
 game1.preGameSetUp()
 print(f'pregame set up: \n{game1}')
 game1.upMove()
@@ -148,4 +161,39 @@ print(f'pick two or four: \n{game1}')
 game1.rightMove()
 print(f'right move: \n{game1}')
 game1.pickTwoOrFour()
-print(f'pick two or four: \n{game1}')
+print(f'pick two or four: \n{game1}')'''
+
+'''game = App2048()
+game.define([[0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2]])
+print(game)
+game.fullUp()
+print(game)
+game.define([[0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2]])
+game.fullDown()
+print(game)
+game.define([[0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2]])
+game.fullLeft()
+print(game)
+game.define([[0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2], [0, 0, 0, 2]])
+game.fullRight()
+print(game)'''
+
+'''game = App2048()
+game.preGameSetUp()
+print(game)
+game.fullUp()
+print(game)
+game.fullDown()
+print(game)
+game.fullLeft()
+print(game)
+game.fullRight()
+print(game)
+game.fullUp()
+print(game)
+game.fullDown()
+print(game)
+game.fullLeft()
+print(game)
+game.fullRight()
+print(game)'''
