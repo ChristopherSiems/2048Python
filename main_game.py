c013b7d9b2fc4de82d000dcf047e56b5e5a72918
Sai Nikhil Chanda <130773971+SaiNikhilChanda@users.noreply.github.com>
2023-04-21 11:37:53 -0400

    running program

--- main_game.py
+++ main_game.py
@@ -6,10 +6,11 @@ from pygame.locals import *
 
 # set up pygame for main gameplay
 pygame.init()
-c = json.load(open("formats.json", "r"))
-screen = pygame.display.set_mode((c["size"], c["size"]))
-my_font = pygame.font.SysFont(c["font"], c["font_size"], bold=True)
-WHITE = (255, 255, 255)
+file = open("formats.json", mode = "r")
+formats = json.load(file)
+screen = pygame.display.set_mode((formats["size"], formats["size"]))
+my_font = pygame.font.SysFont(formats["font"], formats["font_size"], bold=True)
+
 
 def newGame():
     global game1
@@ -18,21 +19,20 @@ def newGame():
     display(game1)
 
 def display(board):
-    screen.fill(tuple(c["colors"]["background"]))
-    box = c["size"] // 4
-    padding = c["padding"]
+    screen.fill(tuple(formats["colors"]["background"]))
+    box = formats["size"] // 4
+    padding = formats["padding"]
     for i in range(4):
         for j in range(4):
-            color = tuple(c["color"][str(board[i][j])])
+            color = tuple(formats["colors"][str(game1.board[i][j])])
             pygame.draw.rect(screen, color, (j * box + padding,
                                              i * box + padding,
                                              box - 2 * padding,
                                              box * 2 - padding), 0)
-            if board[i][j] == 0:
-                text_color = tuple(c["color"]["0"])
+            if game1.board[i][j] == 0:
+                text_color = tuple(formats["colors"]["0"])
             
-            screen.blit(my_font.render('{:>4}'.format(board[i][j]), 1, text_color), 
-                        (j * box + 5 * padding, i * box + 14 * padding))
+            screen.blit(my_font.render('{:>4}'.format(game1.board[i][j]), 1, text_color), (j * box + 5 * padding, i * box + 14 * padding))
     pygame.display.update()
 
 def playGame():
@@ -43,23 +43,34 @@ def playGame():
                 sys.exit()
             
             if event.type == pygame.KEYDOWN:
-                if str(event.key) not in c['buttons']:
+                if str(event.key) not in formats['buttons']:
                     return "please type a valid key"
                 else:
-                    key = c['buttons'][str(event.key)]
+                    key = formats['buttons'][str(event.key)]
                     if key == 'w':
                         game1.fullUp()
+                        display(game1)
                     elif key == 'a':
                         game1.fullLeft()
+                        display(game1)
                     elif key == 's':
                         game1.fullDown()
+                        display(game1)
                     elif key == 'd':
                         game1.fullRight()
+                        display(game1)
                 
                 #checking status of the game
                 if game1.check():
                     game1.pickTwoOrFour()
 
+
+if __name__ == '__main__':
+    json.load(open('formats.json', mode = 'r'))
+    file.close()
+    pygame.init()
+    newGame()
+    playGame()
 
 
             
